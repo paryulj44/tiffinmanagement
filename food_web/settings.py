84fb3c4b0235bbe1509d2ded9ac079ef1a574a10
saranshj44 <saranshj786@gmail.com>
2021-05-19 14:29:34 +0530

    sec

--- food_web/settings.py
+++ food_web/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 import os
-import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -125,6 +125,3 @@ MEDIA_URL= '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [BASE_DIR / 'static']
-
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
-django_heroku.settings(locals())
